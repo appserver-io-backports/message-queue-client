b62bab22d95213dff86efc27cb7a23207fddf7da
wickb <b.wick@techdivision.com>
2014-02-07 16:05:10 +0100

    refactoring of annotations

--- src/TechDivision/MessageQueueClient/QueueSession.php
+++ src/TechDivision/MessageQueueClient/QueueSession.php
@@ -1,12 +1,12 @@
 <?php
-	 
+
 namespace TechDivision\MessageQueueClient;
 
 use \TechDivision\MessageQueueClient\Queue;
 use \TechDivision\MessageQueueClient\QueueSender;
 use \TechDivision\MessageQueueClient\QueueConnection;
 use \TechDivision\MessageQueueClient\Interfaces\Message;
-	 
+
 /**
  * @package	mqclient
  * @author	wagnert <tw@struts4php.org>
@@ -21,7 +21,7 @@ class QueueSession {
  	 * @var \TechDivision\MessageQueueClient\QueueConnection
  	 */
 	protected $connection = null;
-	
+
 	/**
 	 * Holds the unique session id.
 	 * @var string
@@ -31,9 +31,9 @@ class QueueSession {
 	/**
 	 * Initializes the session with the QueueConnection instance
 	 * to use for the server connection.
-	 * 
+	 *
 	 * @param \TechDivision\MessageQueueClient\QueueConnection $connection Holds the QueueConnection instance to use
-	 * @return void
+	 * @return string
 	 */
 	public function __construct(QueueConnection $connection) {
 		// initialize the internal connection
@@ -45,9 +45,9 @@ class QueueSession {
 	/**
 	 * Sends the passed Message instance to the server,
 	 * using the QueueConnection instance.
-	 * 
+	 *
 	 * @param \TechDivision\MessageQueueClient\Interfaces\Message $message The message to send
-	 * @param boolean $validateResponse If this flag is true, the QueueConnection waits for the MessageQueue response and validates it 
+	 * @param boolean $validateResponse If this flag is true, the QueueConnection waits for the MessageQueue response and validates it
 	 * @return \TechDivision\MessageQueueClient\QueueResponse The response of the MessageQueue, or null
 	 */
 	public function send(Message $message, $validateResponse) {
@@ -55,22 +55,22 @@ class QueueSession {
 	}
 
 	/**
-	 * Creates and returns a new QueueSender instance for sending 
+	 * Creates and returns a new QueueSender instance for sending
 	 * the Message to the server.
-	 * 
+	 *
 	 * @param \TechDivision\MessageQueueClient\Queue $queue the Queue instance to use for sending the message
 	 * @return \TechDivision\MessageQueueClient\QueueSender The initialized QueueSender instance
 	 */
 	public function createSender(Queue $queue) {
 		return new QueueSender($this, $queue);
 	}
-	
+
 	/**
 	 * Returns the session id.
-	 * 
-	 * @return integer The unique id
+	 *
+	 * @return string The unique id
 	 */
 	public function getId() {
-		return $this->id;	
+		return $this->id;
 	}
 }
